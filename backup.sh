e26b3c01e6116eeafc52f8dd57d6d0f6eae75b18
Alex Ball <adbsjb@gmail.com>
2025-04-08 11:42:43 +0100

    Replace mylar with kapowarr

--- backup.sh
+++ backup.sh
@@ -3,4 +3,8 @@ echo Enter name of backup:
 
 read backupName
 
-borg create --stats --progress --compression lz4 /mnt/c/Users/adbsj/OneDrive/Documents/ServerBackups/::$backupName /mnt/c/Servers
+borg create --stats --progress \
+  --compression lz4 \
+  --exclude /mnt/c/Servers/jellyfin/library/cache \
+  --exclude /mnt/c/Servers/jellyfin/library/data/metadata/People \
+  /mnt/c/Users/adbsj/OneDrive/Documents/ServerBackups/::$backupName /mnt/c/Servers
