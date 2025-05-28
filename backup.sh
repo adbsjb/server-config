8ace3bfaccba0b73789095087828b27b26b7b3ac
Alex Ball <adbsjb@gmail.com>
2025-05-28 17:40:27 +0100

    Update backup script to work more automatically

--- backup.sh
+++ backup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-echo Enter name of backup:
+echo Latest Backups:
+echo
+./listBackup.sh
 
+echo
+echo Enter name of backup:
 read backupName
 
+echo
+echo Pausing all containers
+docker pause $(docker ps -q)
+
+echo
+echo Creating Backup
 borg create --stats --progress \
   --compression lz4 \
   --exclude ~/Servers/jellyfin/library/cache \
   --exclude ~/Servers/jellyfin/library/data/metadata/People \
   ~/ServerBackup/::$backupName ~/Servers
 
+echo
+echo Resuming all containers
+docker unpause $(docker ps -q)
+
+echo
+echo Syncing to OneDrive
 rclone sync --progress ~/ServerBackup/ remote:/ServerBackup/
